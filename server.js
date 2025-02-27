89ab2f13a3d46776f8572e591d1f2f5addfeec8b
ADSAWINEEEIEI01 <ADSAWINEEWISUT@GMAIL.COM>
2025-02-28 00:10:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.get('/', (req, res) => {
 
 // สร้างการเชื่อมต่อ MySQL
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost', 
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '', 
-  database: process.env.DB_NAME || 'healthtrack',
-  port: process.env.DB_PORT || 3306 // ใช้ค่า port จาก .env ถ้ามี
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // ตรวจสอบการเชื่อมต่อ
